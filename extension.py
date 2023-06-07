a44cf337183ec601fd7abb236764ccceddd583e3
akshit792 <akshit792@gmail.com>
2023-06-07 13:37:30 -0500

    Added options for minute data

--- extension.py
+++ extension.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from zipline.data.bundles import register, csv_data
 
-start_session = pd.Timestamp('2018-01-05', tz='utc')
-end_session = pd.Timestamp('2020-07-31', tz='utc')
+start_session = pd.Timestamp('2020-12-07 09:30:00', tz='utc')
+end_session = pd.Timestamp('2020-12-15 14:05:10', tz='utc')
 register(
-	'csv_data', 
+	'csv_data-minute',
 	csv_data.csv_data, 
 	calendar_name = 'NYSE', 
 	start_session=start_session,
