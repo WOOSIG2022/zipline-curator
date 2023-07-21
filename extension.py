cd22c65eaf562c80ffe119693ed268af2792f694
Zhen Liu <900910@gmail.com>
2023-07-21 12:44:59 -0400

    Update extension.py

--- extension.py
+++ extension.py
@@ -1,9 +1,21 @@
 import pandas as pd
-from zipline.data.bundles import register, csv_NYSE, csvdir
-start_session = pd.Timestamp('2020-12-07 09:30:00', tz='utc')
-end_session = pd.Timestamp('2020-12-15 14:05:10', tz='utc')
-register('csv_data',
-		csv_NYSE.csv_NYSE(tframes=["minute"], csvdir="/Users/Akshit/PycharmProjects/woosig2022/zipline-curator/data/"),
-		calendar_name = 'NYSE',
-		start_session=start_session,
-		end_session=end_session)
+from zipline.data.bundles.csvdir import csvdir_equities
+
+import warnings
+warnings.filterwarnings('ignore')
+
+start_session = pd.Timestamp('2020-12-07', tz='utc')
+end_session = pd.Timestamp('2020-12-15', tz='utc')
+
+register(
+ 	'csv_data_minute',
+ 	csvdir_equities(
+ 		['minute'],
+ 		r'C:\Users\shopp\Box\WOOatUB\ZhenProjects\Zipline_TradingEvolved\data\csv_data', # Change it to the path one level above your minute folder
+ 		),
+	
+	
+	calendar_name = 'NYSE', 
+	start_session=start_session,
+	end_session=end_session
+	)
